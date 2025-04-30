5fe89c34c7b2747f3d9b0783904ac237a859517f
apykwan <apykwan@hotmail.com>
2025-04-30 16:53:10 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <title>Welcome to Soosle</title>
+</head>
+
+<body>
+  <div class="wrapper indexPage">
+    <section class="mainSection">
+      <div class="logoContainer">
+        <img src="https://www.google.com/logos/doodles/2025/celebrating-cherry-blossom-season-copy-6753651837110757-law.gif">
+      </div>
+
+      <div class="searchContainer">
+        <form action="search.php" method="GET">
+          <input class="searchBox" type="text" name="term" />
+          <input class="searchButton" type="submit" value="search" />
+        </form>
+      </div>
+    </section>
+  </div>
+</body>
+
+</html>
