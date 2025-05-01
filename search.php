e5c42786c6134c4b54b88ccbbbe35cf3f15fca99
apykwan <apykwan@hotmail.com>
2025-05-01 02:39:24 -0700

    completed search bar for the search.php

--- search.php
+++ search.php
@@ -1,3 +1,36 @@
-<?php
-  echo $_GET["term"];
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <title>Welcome to Soosle</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="header">
+      <div class="headerContent">
+        <div class="logoContainer">
+          <a href="index.php">
+            <img src="assets/images/soosle_logo.png">
+          </a>
+        </div>
+
+        <div class="searchContainer">
+          <form action="search.php" method="GET">
+            <div class="searchBarContainer">
+              <input class="searchBox" type="text" name="term">
+              <button class="searchButton">
+                <img src="assets/images/search_icon.png">
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
