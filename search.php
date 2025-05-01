aa2f3935ba3cf39c25fef929bce3f32a673f4f7b
apykwan <apykwan@hotmail.com>
2025-05-01 04:43:12 -0700

    completed sites or images search menu

--- search.php
+++ search.php
@@ -1,3 +1,15 @@
+<?php
+if (isset($_GET['term'])) {
+  $term = $_GET['term'];
+  echo $term;
+} else {
+  exit("you must enter search term");
+}
+
+$type = isset($_GET['type']) ? $_GET['type'] : 'sites';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,6 +40,17 @@
             </div>
           </form>
         </div>
+      </div>
+
+      <div class="tabsContainer">
+        <ul class="tabList">
+          <li class='<?php echo $type == "sites" ? 'active' : ''; ?>'>
+            <a href='<?php echo "search.php?term={$term}&type=sites"; ?>'>Sites</a>
+          </li>
+          <li class='<?php echo $type == "images" ? 'active' : ''; ?>'>
+            <a href='<?php echo "search.php?term={$term}&type=images"; ?>'>Images</a>
+            </li>
+        </ul>
       </div>
     </div>
   </div>
